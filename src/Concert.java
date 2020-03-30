b0e5fd23730174bda830d2d55b66d5323c8e50c8
lablonczy <lukeablonczy@hotmail.com>
2020-03-30 15:00:11 -0400

    Movie, Concert started

---
+++ src/Concert.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+
+public class Concert extends Show{
+
+	private ArrayList<String> majorActors;
+
+	public Concert(){
+
+	}
+
+	public Concert(String name, int ageRating, ArrayList<String> majorActors, ArrayList<String> producers){
+		super.setName(name);
+		super.setAgeRating(ageRating);
+		super.setProducers(producers);
+		this.setMajorActors(majorActors);
+	}
+
+	public void addMajorActor(String actor){
+		if(actor == null){
+			System.out.println("Cannot add null actor...");
+			return;
+		}
+
+		if(majorActors.contains(actor)){
+			System.out.println("Actor " + actor + " is already in list...will not add again");
+			return;
+		}
+
+		majorActors.add(actor);
+	}
+
+	public void removeMajorActor(String actor){
+		if(actor == null){
+			System.out.println("Can't remove null actor...");
+			return;
+		}
+
+		if(!actor.contains(actor)){
+			System.out.println("Actor " + actor + " not in list...not removed");
+			return;
+		}
+
+		System.out.println("Removing actor " + actor);
+		majorActors.remove(actor);
+	}
+
+
+	public ArrayList<String> getMajorActors() {
+		return majorActors;
+	}
+
+	public void setMajorActors(ArrayList<String> majorActors) {
+		if(majorActors == null){
+			System.out.println(this + " can't set majorActors bc it is null");
+			return;
+		}
+
+		this.majorActors = majorActors;
+	}
+
+
+	@Override
+	public String toString() {
+		StringBuilder actorsString = new StringBuilder();
+		for(String actor : majorActors)
+			actorsString.append(actor).append(", ");
+
+		return super.toString() + " Famous Actors: " + actorsString;
+	}
+}
