f389c3208ab932103af41ac815a729a819a34226
lablonczy <lukeablonczy@hotmail.com>
2020-03-30 15:05:03 -0400

    Concert.java functional

--- src/Concert.java
+++ src/Concert.java
@@ -2,69 +2,69 @@ import java.util.ArrayList;
 
 public class Concert extends Show{
 
-	private ArrayList<String> majorActors;
+	private ArrayList<String> performers;
 
 	public Concert(){
 
 	}
 
-	public Concert(String name, int ageRating, ArrayList<String> majorActors, ArrayList<String> producers){
-		super.setName(name);
-		super.setAgeRating(ageRating);
-		super.setProducers(producers);
-		this.setMajorActors(majorActors);
+	public Concert(String name, int ageRating, ArrayList<String> performers, ArrayList<String> producers){
+		this.setName(name);
+		this.setAgeRating(ageRating);
+		this.setProducers(producers);
+		this.setPerformers(performers);
 	}
 
-	public void addMajorActor(String actor){
-		if(actor == null){
+	public void addPerformer(String performer){
+		if(performer == null){
 			System.out.println("Cannot add null actor...");
 			return;
 		}
 
-		if(majorActors.contains(actor)){
-			System.out.println("Actor " + actor + " is already in list...will not add again");
+		if(performers.contains(performer)){
+			System.out.println("Performer " + performer + " is already in list...will not add again");
 			return;
 		}
 
-		majorActors.add(actor);
+		performers.add(performer);
 	}
 
-	public void removeMajorActor(String actor){
-		if(actor == null){
-			System.out.println("Can't remove null actor...");
+	public void removePerformer(String performer){
+		if(performer == null){
+			System.out.println("Can't remove null performer...");
 			return;
 		}
 
-		if(!actor.contains(actor)){
-			System.out.println("Actor " + actor + " not in list...not removed");
+		if(!performer.contains(performer)){
+			System.out.println("performer " + performer + " not in list...not removed");
 			return;
 		}
 
-		System.out.println("Removing actor " + actor);
-		majorActors.remove(actor);
+		System.out.println("Removing performer " + performer);
+		performers.remove(performer);
 	}
 
 
-	public ArrayList<String> getMajorActors() {
-		return majorActors;
+	public ArrayList<String> getPerformers() {
+		return performers;
 	}
 
-	public void setMajorActors(ArrayList<String> majorActors) {
-		if(majorActors == null){
-			System.out.println(this + " can't set majorActors bc it is null");
+	public void setPerformers(ArrayList<String> performers) {
+		if(performers == null){
+			System.out.println(this + " can't set performers bc it is null");
 			return;
 		}
 
-		this.majorActors = majorActors;
+		this.performers = performers;
 	}
 
 
 	@Override
 	public String toString() {
-		StringBuilder actorsString = new StringBuilder();
-		for(String actor : majorActors)
-			actorsString.append(actor).append(", ");
+		StringBuilder performersString = new StringBuilder();
+		for(String actor : performers)
+			performersString.append(actor).append(", ");
 
-		return super.toString() + " Famous Actors: " + actorsString;
+		return super.toString() + " Performers: " + performersString;
 	}
 }
