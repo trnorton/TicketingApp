875af7d558a7819963d0ace7661700b563ae58fa
Taylor Norton <taylornorton@Taylors-MacBook-Pro.local>
2020-04-04 15:27:16 -0400

    Updated

--- src/Theater.java
+++ src/Theater.java
@@ -27,7 +27,12 @@ public class Theater {
 	}
 
 	public void displaySeats() {
+		System.out.println("  ");
+		for(int i = 0; i<seats[0].length;i++)
+			System.out.print(" " + (i+1) + " ");
+		
 		for (int i = 0; i < seats.length; i++) {
+			System.out.print(alphabet[i] + " ");
 			for (int j = 0; j < seats[i].length; j++) {
 				if (seats[i][j].checkIfTaken() == true)
 					seats[i][j].setSeatStatus("X");
