aafb64170b3b13f7ba84f1aaf4a0b1e2299b0fc9
Taylor Norton <taylornorton@Taylors-MacBook-Pro.local>
2020-04-03 21:17:15 -0400

    Added main system skeleton

---
+++ src/MainSystem.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+
+public class MainSystem {
+
+	private ArrayList<Venue> venues;
+	private ArrayList<User> users;
+	private FilterSystem filter;
+	private CustomerSupportSystem custSupport;
+	private ConcessionsSystem concess;
+	private boolean online;
+	
+	public MainSystem() {
+		
+	}
+	
+	public void searchForEvent(String event) {
+		
+	}
+	
+	public void searchForLocation(String location) {
+		
+	}
+	
+	private void applySearchFilters() {
+		
+	}
+	
+	private void applyRestrictionFilters() {
+		
+	}
+	
+	public boolean checkIfOnline() {
+		return true;
+	}
+	
+	public void displaySupportInfo() {
+		
+	}
+	
+	public void distributeConcessions() {
+		
+	}
+	
+	public void displayConcessions() {
+		
+	}
+}
