d3ad178d3d98681d9ca000be63de7eeb313ee294
lablonczy <lukeablonczy@hotmail.com>
2020-04-05 00:11:39 -0400

    Employee Javadoc complete

--- src/Employee.java
+++ src/Employee.java
@@ -22,6 +22,14 @@ public class Employee extends User {
 		this.setWorkVenue(workVenue);
 	}
 
+	/**
+	 * Allows an employee to create an event for a venue
+	 * @param venueName The name of the venue in which to place the event
+	 * @param venuesToSearch  The list of venues from which to find the matching venue
+	 * @param date The date of the event in ddmmyy?
+	 * @param time The time of the event in 12hr h:m format
+	 * @param show The show type to add
+	 */
 	public void inputEvent(String venueName, ArrayList<Venue> venuesToSearch, String date, String time, Show show){
 		//find venue mainsystem
 		Venue venueToAddEventTo = null;
@@ -44,26 +52,29 @@ public class Employee extends User {
 
 	}
 
-	public void inputTheaterInfo(String theater, String type, String show, String date, String time){
 
-	}
+	/*public void inputTheaterInfo(String theater, String type, String show, String date, String time){
 
+	}*/
+
+	/**
+	 * Sets the discount of each of a user's tickets to a certain multiplier. End result price will be (price * multiplier) aka (price * (discount/100))
+	 * @param discount The percentage discount to give as a double
+	 * @param user The user to allow that discount
+	 */
 	public void inputDiscount(double discount, User user){
-		for(Ticket ticket : user.getTickets())
-			ticket.setDiscountMultiplier(discount/100);
+		user.getTickets().foreach((ticket) -> ticket.setDiscountMultiplier(discount/100));
 	}
 
 	/*public void inputTicketPrices(double adultPrice, double childPriceMultiplier, ){
 
 	}*/
 
-
-	public void displayRefundedTickets(){
-		System.out.println("Tickets Refunded: " + ticketsToRefundToCustomers.size());
-		ticketsToRefundToCustomers.forEach(System.out::println);
-	}
-
-	public void addTicket(Ticket ticket){
+	/**
+	 * Allows employee to add a ticket into the list of tickets to refund
+	 * @param ticket Any ticket object
+	 */
+	public void addTicketToRefund(Ticket ticket){
 		if(ticket == null){
 			System.out.println("Cannot add null ticket");
 			return;
@@ -72,31 +83,64 @@ public class Employee extends User {
 		ticketsToRefundToCustomers.add(ticket);
 	}
 
+	/**
+	 * Refunds a customer's tickets and displays the refunded tickets and price
+	 * @param refundRequester The String name of a user requesting a refund
+	 */
 	public void refundTickets(String refundRequester){
-		//Print name
-		//make list of tickets belonging to user only
-		//do magic
-		//print out total tickets refunded
-		//
+		//ArrayList<Ticket> tickets = ticketsToRefundToCustomers.stream().filter((t) -> t.getName().equals(refundRequester)).collect(Collectors.toCollection(ArrayList::new));
+		//Look at what I made ^^
+
+		ArrayList<Ticket> tickets = new ArrayList<>();
+		for(Ticket t : ticketsToRefundToCustomers)
+			if(t.getName().equalsIgnoreCase(refundRequester))
+				tickets.add(t);
+
+		System.out.print("Refunding "+ refundRequester + " " + tickets.size() + " tickets ");
+		int totalTicketPrice = 0;
+		for(Ticket t: tickets)
+			totalTicketPrice += t.getPriceWithDiscount();
+		System.out.println("for a total amount of " + totalTicketPrice + " dollars!!");
+
 
 	}
 
+	/**
+	 * Creates string representation of Employee
+	 * @return Employee Name: Luke Venue: Regal 18 Birthday: 01011987 Phone: 9078675309 Home Address: 123 Fake Street Email: bloopy@gmail.com
+	 */
 	public String toString(){
 		return "Employee\tName:" + this.getName() + "\nVenue: " + this.getWorkVenue() + "\nBirthday: " + this.getBirthday() + "\nPhone:  " + this.getPhoneNumber() + "\nHome Address: " + this.getAddress() + "\nEmail: " + this.getEmail();
 	}
 
+	/**
+	 * Returns the venue the employee works at
+	 * @return A venue object
+	 */
 	public Venue getWorkVenue() {
 		return workVenue;
 	}
 
+	/**
+	 * Sets the venue which the employee works at
+	 * @param workVenue A venue object to assign to the employee
+	 */
 	public void setWorkVenue(Venue workVenue) {
 		this.workVenue = workVenue;
 	}
 
+	/**
+	 * An arraylist of tickets that will be refunded
+	 * @return The arraylist of ticket objects to be refunded
+	 */
 	public ArrayList<Ticket> getTicketsToRefundToCustomers() {
 		return ticketsToRefundToCustomers;
 	}
 
+	/**
+	 * Allows assigning of an arraylist of ticket objects to an employee
+	 * @param ticketsToRefundToCustomers An arraylist of ticket objects to assign to an employee
+	 */
 	public void setTicketsToRefundToCustomers(ArrayList<Ticket> ticketsToRefundToCustomers) {
 		this.ticketsToRefundToCustomers = ticketsToRefundToCustomers;
 	}
