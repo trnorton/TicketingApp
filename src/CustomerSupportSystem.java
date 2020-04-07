713ae87a457417fe81a13c3e115d984cdb4ba1d7
Taylor Norton <taylornorton@Taylors-MacBook-Pro.local>
2020-04-06 21:47:11 -0400

    Updated

--- src/CustomerSupportSystem.java
+++ src/CustomerSupportSystem.java
@@ -1,14 +1,27 @@
-
+/**
+ * CustomerSupportSystem.java - A class to represent a customer support page.
+ * 
+ * @author Taylor Norton
+ *
+ */
 public class CustomerSupportSystem {
 
 	private String description;
 	private int phoneNumber;
 
+	/**
+	 * Default Constructor.
+	 */
 	public CustomerSupportSystem() {
 		description = "This is the customer support page. If you are experiencing issues with the app, call the number below:";
 		phoneNumber = 1234567890;
 	}
-	
+
+	/**
+	 * Returns a String representation of the CustomerSupportSystem.
+	 * 
+	 * @return String - The representation of the CustomerSupportSystem.
+	 */
 	public String toString() {
 		return description + "\n" + phoneNumber;
 	}
